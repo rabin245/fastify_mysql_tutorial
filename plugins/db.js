1dd54aba74c225e35f184f56791ef0cf3c53e80f
rabin245 <drarabin@gmail.com>
2023-05-20 20:39:27 +0545

    use env variables for db connection

--- plugins/db.js
+++ plugins/db.js
@@ -1,13 +1,16 @@
 import fastifyPlugin from "fastify-plugin";
 import mysql from "@fastify/mysql";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConnector = async (fastify, options, done) => {
   fastify.register(mysql, {
     promise: true,
-    host: "localhost",
-    password: "Sqlp@ssw0rd",
-    user: "zaxiya",
-    database: "fastify_test",
+    host: process.env.MYSQL_HOST,
+    password: process.env.MYSQL_PASSWORD,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
     port: 3306,
   });
 
